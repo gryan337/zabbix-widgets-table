c7f92da1cc35045b5789ce6b942df73c26df4de6
Ryan Eberly <reberly337@gmail.com>
2026-01-04 22:12:17 -0500

    Issue #35 add back mistakenly deleted filter type change event handler

--- assets/js/class.widget.js
+++ assets/js/class.widget.js
@@ -1682,6 +1682,10 @@ class CWidgetTableModuleRME extends CWidget {
 			updateClearFiltersButton();
 		};
 
+		filterTypeSelect.addEventListener('filterTypeChanged', (e) => {
+			handleInput();
+		});
+
 		// Store handleInput reference so #updateFilterPopupValues can call it
 		searchInput._handleInputFunction = handleInput;
 
